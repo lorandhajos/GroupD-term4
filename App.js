699a2bb262b7c6519d21efda24dc09e0de0b36c7
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2023-05-11 13:23:41 +0200

    Added ble library

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, NativeModules } from 'react-native';
+import { BleManager } from 'react-native-ble-plx';
+
+const {UsbSerial} = NativeModules;
+
+const manager = new BleManager();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title={"Scan"} onPress={() => {
+        manager.startDeviceScan(null, null, (error, device) => {
+          if (error) {
+            console.warn(error);
+            return
+          }
+        
+          if (device.name != "null") {
+              this.manager.stopDeviceScan();
+              console.log(device.name);
+          }
+        });
+      }} />
+      <Button title={"Usb"} onPress={() => {
+        UsbSerial.openDevice()
+      }} />
       <StatusBar style="auto" />
     </View>
   );
