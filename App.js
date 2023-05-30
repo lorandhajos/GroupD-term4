168ce9c8248b9d2f0646e5ca8d793e44761f83eb
iamjames5 <lennox.james@student.nhlstenden.com>
2023-05-30 14:10:10 +0200

    Setup screen

--- App.js
+++ App.js
@@ -86,48 +86,7 @@ function FirstTimeSetupScreen({navigation}) {
 }
 
 function App() {
-  const [appIsLoaded, setAppIsLoaded] = useState(false);
-
-  useEffect(() => {
-    
-  const prepare = async () => {
-    try {
-      await Font.loadAsync({
-        "black": require("./assets/fonts//Roboto-Black.ttf"),
-        "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
-        "bold": require("./assets/fonts/Roboto-Bold.ttf"),
-        "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
-        "italic": require("./assets/fonts/Roboto-Italic.ttf"),
-        "light": require("./assets/fonts/Roboto-Light.ttf"),
-        "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
-        "medium": require("./assets/fonts/Roboto-Medium.ttf"),
-        "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
-        "regular": require("./assets/fonts/Roboto-Regular.ttf"),
-        "thin": require("./assets/fonts/Roboto-Thin.ttf"),
-          "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
-      });
-    }
-    catch (error) {
-      console.log.error();
-    }
-    finally {
-      setAppIsLoaded(true);
-    }
-  };
-
-  prepare();
-  }, []);
-
-  const onLayout = useCallback(async () => {
-  if (appIsLoaded) {
-    await SplashScreen.hideAsync();
-  }
-  }, [appIsLoaded]);
-
-  if (!appIsLoaded) {
-  return null;
-  }
-
+  
   return (
     <SafeAreaProvider>
       <StatusBar style="auto" />
