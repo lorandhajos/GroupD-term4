d7819f9a10240676ee832ff2bd570c994e62ede7
iamjames5 <lennox.james@student.nhlstenden.com>
2023-05-30 14:45:02 +0200

    SetupScreen code is in its own file

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Picker } from '@react-native-picker/picker';
 import { useState, useEffect } from 'react';
 import { HomeScreen, DetailsScreen } from './components'
+import SetupScreen from './components/SetupScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -30,30 +31,6 @@ function ChangeRegionScreen({navigation}){
   );
 }
 
-function FirstTimeSetupScreen({navigation}) {
-
-  const image = require('./assets/setupImage.png');
-
-  return (
-    <View style={styles.setupScreen}>
-      <ImageBackground source={image} resize='cover' style={styles.setupImage}>
-        <Pressable style={styles.communicationButton} onPress={ () => navigation.navigate('Home')}>
-          <Text style={styles.startCommunication}>Start communication</Text>
-        </Pressable>
-        <Pressable style={styles.settingButton} onPress={ () => navigation.navigate('Home')}>
-          <Text style={styles.startCommunication}>Settings</Text>
-        </Pressable>
-        <Text style={styles.inspirationQuote}>“JOBS FILL YOUR POCKETS, BUT ADVENTURES FILL YOUR SOUL.”</Text>
-        <Pressable style={styles.sosButton} onPress={() =>
-           <Button title='Send'/>}>
-          <Text style={styles.sosText}>SOS</Text>
-        </Pressable>
-      </ImageBackground>
-
-    </View>
-  );
-}
-
 function App() {
   
   return (
@@ -61,7 +38,7 @@ function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator screenOptions={{animation: "slide_from_right"}}>
-          <Stack.Screen name="Setup Screen" component={FirstTimeSetupScreen} />
+          <Stack.Screen name="Setup Screen" component={SetupScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Details" component={DetailsScreen} options={({ route }) => ({ title: route.params.name })}/>
           <Stack.Screen name="ChangeRegion" component={ChangeRegionScreen}/>
@@ -71,4 +48,3 @@ function App() {
   );
 }
 
-export default App;
---
+++ components/SetupScreen.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+function SetupScreen({navigation}) {
+
+    const image = require('./assets/setupImage.png');
+  
+    return (
+        <View style={styles.container}>
+            <View style={styles.setupScreen}>
+                <ImageBackground source={image} resize='cover' style={styles.setupImage}>
+                    <Pressable style={styles.communicationButton} onPress={ () => navigation.navigate('Home')}>
+                        <Text style={styles.startCommunication}>Start communication</Text>
+                    </Pressable>
+                    <Pressable style={styles.settingButton} onPress={ () => navigation.navigate('Home')}>
+                        <Text style={styles.startCommunication}>Settings</Text>
+                    </Pressable>
+                    <Text style={styles.inspirationQuote}>“JOBS FILL YOUR POCKETS, BUT ADVENTURES FILL YOUR SOUL.”</Text>
+                    <Pressable style={styles.sosButton} onPress={() =>
+                        <Button title='Send'/>}>
+                        <Text style={styles.sosText}>SOS</Text>
+                    </Pressable>
+                </ImageBackground>
+            </View>
+        </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#f5f5f5',
+    },
+    name: {
+      fontWeight: 'bold',
+      fontSize: 18,
+    },
+    time: {
+      height: '100%',
+    },
+    setupScreen:{
+      flex: 1,
+    },
+  
+    setupImage: {
+      flex: 1,
+    },
+    communicationButton:{
+      backgroundColor: '#f5f5f5',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'black',
+      height: '5%',
+      width: '60%',
+      marginLeft:'20%',
+      marginVertical: '10%',
+    },
+    settingButton:{
+      backgroundColor: '#f5f5f5',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'black',
+      height: '5%',
+      width: '60%',
+      marginLeft:'20%',
+    },
+    startCommunication:{
+      fontWeight: 'bold',
+      fontSize: 20,
+    },
+    inspirationQuote: {
+      flex: 1,
+      color: 'white',
+      alignItems: 'center',
+      fontSize: 28,
+      position: 'absolute',
+      textAlign: 'center',
+      width: '50%',
+      marginLeft:'20%',
+      marginTop: 300,
+      
+    },
+    sosButton:{
+      backgroundColor: '#f5f5f5',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 50,
+      height: 100,
+      width: '30%',
+      marginVertical: '100%',
+      marginLeft: 30,
+      marginLeft:'35%',
+      backgroundColor: 'red',
+      
+    },
+    sosText:{
+      color: 'white',
+      fontSize: 30,
+      fontWeight: 'bold',
+    }
+  });
+  export default SetupScreen;
+  
