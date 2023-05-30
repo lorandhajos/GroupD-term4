8bcdc7bba9a18476054745f44e09fba9edddcc77
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2023-05-30 15:10:47 +0200

    Minor styling changes to the SetupScreen

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ function App() {
       <StatusBar style="auto" />
       <NavigationContainer>
         <Stack.Navigator screenOptions={{animation: "slide_from_right"}}>
-          <Stack.Screen name="Setup Screen" component={SetupScreen} />
+          <Stack.Screen name="SetupScreen" component={SetupScreen} />
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Details" component={DetailsScreen} options={({ route }) => ({ title: route.params.name })}/>
           <Stack.Screen name="ChangeRegion" component={ChangeRegionScreen}/>
