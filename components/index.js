59ffdc02e632c08e87a0635ab01e91bdead8cf8a
RaresManoli <rares.manoli@student.nhlstenden.com>
2023-05-30 16:17:42 +0200

    separating the changeRegion screen into a separate file and cleaning up redundant code

--- components/index.js
+++ components/index.js
@@ -1,5 +1,6 @@
 import HomeScreen from './HomeScreen'
 import DetailsScreen from './DetailsScreen'
 import SetupScreen from './SetupScreen'
+import ChangeRegion from './ChangeRegion'
 
-export { HomeScreen, DetailsScreen, SetupScreen };
+export { HomeScreen, DetailsScreen, SetupScreen, ChangeRegion };
