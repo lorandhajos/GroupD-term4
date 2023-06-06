a3759d46d855cfb83c14ade91fb42bcfdf9632fe
AlexMednov <alex.mednov@student.nhlstenden.com>
2023-06-06 15:51:34 +0200

    Flags, errors, comands, some functions, proggram outline

--- firmware/Communication/tx/tx.ino
+++ firmware/Communication/tx/tx.ino
@@ -5,33 +5,86 @@
 #define RFM95_INT   7
 #define RFM95_RST   4
 
+//array{comand, adres, flags, message}
+//arrayElse{comand, setToWhat}
+
+constexpr int comandSned = 1;
+constexpr int comandChangeMode = 3;
+constexpr int comandSOS = 9;
+
+constexpr int flagEmpty = 2;
+constexpr int flagSnedConf = 4;
+constexpr int flagNoConf = 8;
+
+constexpr int modeSleep = 5;
+constexpr int modeStandBy = 6;
+constexpr int modeListen = 7;
+constexpr int modeSend = 8;
+
+constexpr int maxRestart = 50;
+
+constexpr int errorUnexpected = 40;
+constexpr int errorFailedStart = 41;
+constexpr int errorFailedToSend = 42;
+constexpr int errorFailedConnect = 43;
+constexpr int errorFailedSerial = 44;
+constexpr int errorFailedChangeFreq = 45;
+constexpr int errorFailedModeChange = 46;
+
 int LED = 13;
 
 void setup(){
+  int restarts = 0;
   pinMode(LED, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
-  while (!Serial); // wait until serial console is open, remove if not tethered to computer
+  while (!Serial){
+    restart++;
+    delay(1);
+    if(restart>maxRestart){
+      trhowErrorToPhone(errorFailedSerial);
+      restart()
+    }
+  }
+
+  // wait until serial console is open, remove if not tethered to computer
   Serial.begin(115200);
   delay(100);
-  //Serial.println("Setup complete");
-  
-  // manual reset
-  //digitalWrite(RFM95_RST, LOW);
-  //delay(10);
-  //digitalWrite(RFM95_RST, HIGH);
-  //delay(10);
-}
-void loop(){
-  digitalWrite(LED, LOW);
-  if(Serial.available()>0){
-    digitalWrite(LED, HIGH);
-    delay(100);
+
+  restarts = 0;
+  while(!rf9.init()){
+    restart++;
+    delay(1);
+    if(restart>maxRestart){
+      trhowErrorToPhone(errorFailedStart);
+      restart()
+    }
   }
 }
+void loop(){
+  if(checkConectioin()){
+    if(checkSerial()){
+          //main
+    }
+  }
+  else{
 
-void checkSerial(){
+  }
+
+
+
+
+
+
+  //digitalWrite(LED, LOW);
+  //if(Serial.available()>0){
+    //digitalWrite(LED, HIGH);
+    //delay(100);
+  //}
+}
+
+bool checkSerial(){
   if(Serial.available()>0){
     return true;
   }
@@ -44,9 +97,31 @@ void checkSerial(){
 //  }
 //}
 
+void setFrequency(){
+
+}
+
 void send(){
 
 }
 void SOS(){
 
+}
+
+bool checkConectioin(){
+  if(Serial){
+    return true;
+  }
+  return false;
+}
+
+void throwErrorToPhone(int errorType){
+
+}
+
+void restart(){
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
 }
