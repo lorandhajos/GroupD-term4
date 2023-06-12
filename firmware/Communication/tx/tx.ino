163244eaca668420effc058ae60932a390030f21
AlexMednov <alex.mednov@student.nhlstenden.com>
2023-06-12 16:51:15 +0200

    Finished refactroring sending sequence

--- firmware/Communication/tx/tx.ino
+++ firmware/Communication/tx/tx.ino
@@ -8,20 +8,6 @@
 
 #define RF95_FREQ 915.0
 
-//char {comand, adres, flags, message}
-//char {comand, setToWhat}
-
-//sendto(addres, message{from, flag, message});
-
-//comands - single digit
-//flags - single digit
-//addres - tripple digit
-
-//max addres is 255
-//initialise manager
-
-//set to what
-
 
 constexpr int comnandSend = 1;
 constexpr int comnandChangeMode = 2;
@@ -35,17 +21,10 @@ const uint8_t BITMASK_ACK_REQ = 0b00000001;
 const uint8_t BITMASK_IS_ACK =  0b00000010;
 const uint8_t BITMASK_IS_KEY =  0b00000100;
 
-constexpr int flagEmpty = 0;
-constexpr int flagSnedConf = 2;
-constexpr int flagNoConf = 1;
-
-
 constexpr int modeSleep = 5;
 constexpr int modeStandBy = 6;
 constexpr int modeListen = 7;
 constexpr int modeSend = 8;
-
-constexpr int maxRestart = 50;
 
 constexpr int errorUnexpected = 40;
 constexpr int errorFailedStart = 41;
@@ -59,54 +38,29 @@ constexpr int errorUnexpectedCommand = 47;
 const int ASCInum[] = {10, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
 const int UTFnum[] = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-int LED = 13;
-
 int count = 0;
 
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 RHDatagram manager(rf95, 13);
 void setup(){
-  int restarts = 0;
-  pinMode(LED, OUTPUT);
 
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
 
   while(!Serial){
-    restarts++;
     delay(1);
-    if(restarts>maxRestart){
-      throwErrorToPhone(errorFailedSerial);
-      restart();
-    }
   }
 
   // wait until serial console is open
   Serial.begin(115200);
   delay(100);
 
-  restarts = 0;
-  while(!rf95.init()){
-    restarts++;
-    delay(1);
-    if(restarts>maxRestart){
-      throwErrorToPhone(errorFailedStart);
-      restart();
-    }
-  }
+  rf95.init();
 
-  if(!rf95.setFrequency(RF95_FREQ)){
-    while(1);
-  }
+  rf95.setFrequency(RF95_FREQ);
 
-  restarts = 0;
   while(!manager.init()){
-    restarts++;
     delay(1);
-    if(restarts>maxRestart){
-      throwErrorToPhone(errorFailedStart);
-      restart();
-    }
   }
   rf95.setTxPower(23, false);
 }
@@ -117,42 +71,33 @@ void loop(){
       count++;
       char buffer[250]; //maximum length of mesage 240
       int len = Serial.readBytes(buffer, 250);
+      delay(100);
       int action = conversion(buffer[0]);
 
-      if(action == comnandSend && count == 1 && len>4){
+      if(action == 1 && len>6 && count==1){
+
         String tmp;
         for(int i=1; i<4; i++){
           tmp.concat(buffer[i]);
         }
         int address = tmp.toInt();
-        count++;
-        if(count==2){
-          int flag = conversion(buffer[4]);
-          count++;
-          if(count==3){
-            String payload;
-            for(int i=5; i<len-1; i++){
-              payload.concat(buffer[i]);
-            }
-            int Plength = sizeof(payload)+2;
-            char toSend[Plength];
-            toSend[Plength-1]='/0';
-            payload.toCharArray(toSend, Plength);
-            manager.setHeaderTo(address);
-            setFlags(true, false, false);
-            manager.sendto((uint8_t *)toSend, Plength, 255);
-            manager.waitPacketSent();
-            //action
-            //address
-            //flag
-            //payload
-            Serial.println("We got here");
-            Serial.println(Plength);
-            Serial.println(address);
-            Serial.println(toSend);
-          }
+
+        int flag = conversion(buffer[4]);
+
+        char loadRead[250];
+        for(int i=5; i<len-1; i++){
+          loadRead[i-5]=buffer[i];
         }
+        int Plength = len-5;
+        char payload[Plength];
+        for(int i=0; i<Plength-1;i++){
+          payload[i] = loadRead[i];
+          Serial.println(payload[i]);
+        }
+        payload[Plength-1]=0;
+        
       }
+      /*
       else if(action == comnandChangeMode && count == 1){
         count++;
         int mode = conversion(buffer[1]);
@@ -198,7 +143,7 @@ void loop(){
             rf95.setFrequency(freq);
           }
       }
-
+      */
       else{
         count++;
         throwErrorToPhone(errorUnexpectedCommand);
