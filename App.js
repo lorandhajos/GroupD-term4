a8ac7ad5219ce68645eaf4d896a3df226d0d2cd4
RaresManoli <rares.manoli@student.nhlstenden.com>
2023-06-12 16:50:18 +0200

    the select region can now store the value selected even if you exit the screen

--- App.js
+++ App.js
@@ -3,27 +3,36 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { HomeScreen, DetailsScreen, SetupScreen, ChangeRegion, Settings, Username } from './components';
+import { HomeScreen, DetailsScreen, SetupScreen, Settings, Username } from './components';
 import { MenuProvider } from 'react-native-popup-menu';
+
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
+    
     <MenuProvider>
       <SafeAreaProvider>
         <StatusBar style="auto" />
         <NavigationContainer>
-          <Stack.Navigator screenOptions={{animation: "slide_from_right"}}>
+          <Stack.Navigator screenOptions={{ animation: "slide_from_right" }}>
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Details" component={DetailsScreen} options={({ route }) => ({ title: route.params.name })}/>
+            <Stack.Screen name="Details" component={DetailsScreen} options={({ route }) => ({ title: route.params.name })} />
             <Stack.Screen name="SetupScreen" component={SetupScreen} />
-            <Stack.Screen name="Settings" component={Settings}/>
+            <Stack.Screen name="Settings" component={Settings} />
+
+          
+
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaProvider>
     </MenuProvider>
-  );
+    
+
+    
+    );
 }
+
 
 export default App;
--- components/Settings.js
+++ components/Settings.js
@@ -1,14 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, Pressable } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { Button } from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Settings = () => {
   const [pickerVisible, setPickerVisible] = useState(false);
-  const [selectedLanguage, setSelectedLanguage] = useState("EU"); // set default selected value 
+  const [selectedLanguage, setSelectedLanguage] = useState("selectedLanguage"); // set default selected value
+  const [selectedRegion, setSelectedRegion] = useState(""); // new state variable for selected region
+
+  useEffect(() => {
+    retrieveSelectedRegion();
+  }, []);
 
   const handlePress = () => {
     setPickerVisible(!pickerVisible);
+  };
+
+  const handleRegionChange = (itemValue) => {
+    setSelectedRegion(itemValue);
+    storeSelectedRegion(itemValue);
+  };
+
+  const storeSelectedRegion = async (value) => {
+    try {
+      await AsyncStorage.setItem('selectedRegion', value);
+    } catch (error) {
+      console.log('Error storing selected region:', error);
+    }
+  };
+
+  const retrieveSelectedRegion = async () => {
+    try {
+      const value = await AsyncStorage.getItem('selectedRegion');
+      if (value !== null) {
+        setSelectedRegion(value);
+      }
+    } catch (error) {
+      console.log('Error retrieving selected region:', error);
+    }
   };
 
   return (
@@ -27,9 +57,9 @@ const Settings = () => {
 
       {pickerVisible && (
         <Picker
-          selectedValue={selectedLanguage}
+          selectedValue={selectedRegion} // Use selectedRegion instead of selectedLanguage
           style={{ height: 20, width: 150 }}
-          onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}
+          onValueChange={handleRegionChange} // Use handleRegionChange function
         >
           <Picker.Item label="Europe" value="EU" />
           <Picker.Item label="Asia" value="AS" />
@@ -41,7 +71,6 @@ const Settings = () => {
         <Image source={require('../assets/notification.png')} />
         <Text style={styles.naming}> Notifications </Text>
       </View>
-
     </View>
   );
 };
@@ -51,18 +80,12 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
   },
-
   row: {
     flexDirection: 'row',
     alignItems: 'center',
   },
-
   naming: {
     fontSize: 22,
-  },
-
-  selectedLanguage: {
-    alignSelf: 'auto',
   },
 });
 
