0d60d8b04e6dad80ca907fc0ad9173b5642f1787
Lorand Hajos <lorand.hajos@student.nhlstenden.com>
2023-06-19 15:48:42 +0200

    Added option to add contacts using qr codes

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import DetailsScreen from './DetailsScreen'
 import SetupScreen from './SetupScreen'
 import Settings from './Settings'
 import db from './Database'
+import AddContactScreen from './AddContactScreen'
 
-export { HomeScreen, DetailsScreen, SetupScreen, Settings, db };
+export { HomeScreen, DetailsScreen, SetupScreen, Settings, db, AddContactScreen };
