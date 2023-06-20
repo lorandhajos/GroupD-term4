9f4b541e4561f5af413bb9e7cbbafc12c78be24f
Andrii Chumakov <andrii.chumakov@student.nhlstenden.com>
2023-06-20 11:36:31 +0200

    added final.ino, implemented remembering last messages instead of first, fixed ack bug

--- firmware/Communication/rx/rx.ino
+++ firmware/Communication/rx/rx.ino
@@ -8,24 +8,27 @@
 
 constexpr double defaultFrequency = 915.0;
 
-const int maxMessages = 15;
+const int maxMessages = 25;
 
+// C-style strings are probably more memory efficient, but an array of an array of chars directly is difficult
+// Convert the String to char[] before sending it over the cable!
 String g_msgArray[maxMessages];
 
 int g_freeIndexMsg = 0;
+bool g_msgArrayFull = false;
 
 double g_currentFrequency = defaultFrequency;
 
-const uint8_t BITMASK_CLEAR_ALL = 0b00001111;
-const uint8_t BITMASK_ACK_REQ   = 0b00000001;
-const uint8_t BITMASK_IS_ACK    = 0b00000010;
-const uint8_t BITMASK_IS_KEY    = 0b00000100;
-const uint8_t BITMASK_IS_SOS    = 0b00001000;
-
+const uint8_t ClearFlagsMask = 0b00001111;
+const uint8_t ReqAckMask     = 0b00000001;
+const uint8_t IsAckMask      = 0b00000010;
+const uint8_t IsKeyMask      = 0b00000100;
+const uint8_t IsSOSMask      = 0b00001000;
 
 // Radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 RHDatagram manager(rf95, 12);
+
 void resetRST() {
     digitalWrite(RFM95_RST, LOW);
     delay(10);
@@ -35,7 +38,7 @@ void resetRST() {
 
 void setFlags(uint8_t flag) {
     // cleaning up all flags
-    manager.setHeaderFlags(RH_FLAGS_NONE, BITMASK_CLEAR_ALL);
+    manager.setHeaderFlags(RH_FLAGS_NONE, ClearFlagsMask);
     // setting the new flag
     manager.setHeaderFlags(flag);
 }
@@ -44,18 +47,22 @@ bool hasFlag(uint8_t flag, uint8_t target) {
     return (flag & target);
 }
 
-bool addMessageToArray(String message) {
+void addMessageToArray(String message) {
     if (g_freeIndexMsg < maxMessages) {
         g_msgArray[g_freeIndexMsg] = message;
         g_freeIndexMsg++;
-        return true;
     }
-    return false;
+    else {
+        g_msgArrayFull = true;
+        g_freeIndexMsg = 0;
+        addMessageToArray(message);
+    }
 }
 
 bool sendRadioMessage(char* buf, int len, int address = 255, int timeout=1000) {
     manager.sendto(buf, len, address);
     return manager.waitPacketSent(timeout);
+    setFlags(0);
 }
 
 bool checkConnection() {
@@ -87,17 +94,45 @@ void sendMessageToPhone(String msg) {
     char buf[255];
     msg.toCharArray(buf, 255);
     Serial.write(buf);
+    Serial.write('\n');
+}
+
+void sendAllMessagesToPhone() {
+    /*
+    if (!g_msgArray && g_freeIndexMsg == 0) {
+        return;
+    }
+    */
+    int index = g_freeIndexMsg + 1;
+    if (g_msgArrayFull) {
+        index = maxMessages;
+    }
+    for (int i=0; i<index; i++) {
+        //Serial.println(i);
+        if (g_msgArray[i].length() > 0) {
+            sendMessageToPhone(g_msgArray[i]);
+            g_msgArray[i] = "";
+        }
+    }
+    g_freeIndexMsg = 0;
+    g_msgArrayFull = false;
+}
+void sendAck(uint8_t id, char* contents, uint8_t address=255) {
+    manager.setHeaderId(id);
+    //rf95.setModeIdle();
+    setFlags(IsAckMask);
+    sendRadioMessage(contents, sizeof(contents), address);
 }
   
 bool getRadioMessage() {
-    rf95.setModeRx();
+    //rf95.setModeRx();
     if (manager.available()) {
         // check if there is a message for us
         char lastMessage[255];
         uint8_t len = sizeof(lastMessage);
         if (manager.recvfrom(lastMessage, &len)) {
             uint8_t msgFlags = manager.headerFlags();
-            if (hasFlag(msgFlags, BITMASK_IS_ACK)) {
+            if (hasFlag(msgFlags, IsAckMask)) {
                 //discard the ackhnowledgment messages
                 return false;
             }
@@ -105,26 +140,18 @@ bool getRadioMessage() {
             String convMessage = convert(sender, msgFlags, lastMessage);
             if (checkConnection()) {
                 sendMessageToPhone(convMessage);
-                if (hasFlag(msgFlags, BITMASK_ACK_REQ)) {
+                if (hasFlag(msgFlags, ReqAckMask)) {
                     delay(100);
-                    manager.setHeaderId(manager.headerId());
-                    rf95.setModeIdle();
-                    sendRadioMessage("0", 2);
+                    sendAck(manager.headerId(), "0", 2);
                 }
                 return true;
             }
-            else {
-                if (addMessageToArray(convMessage)) {
-                    if (hasFlag(msgFlags, BITMASK_ACK_REQ)) {
-                        delay(100);
-                        manager.setHeaderId(manager.headerId());
-                        rf95.setModeIdle();
-                        sendRadioMessage("0", 2);
-                    }
-                    return true;
-                }
-                return false;
+            addMessageToArray(convMessage);
+            if (hasFlag(msgFlags, ReqAckMask)) {
+                delay(100);
+                sendAck(manager.headerId(), "0", 2);
             }
+            return true;
         }
     }
     return false;
@@ -164,14 +191,7 @@ void setup() {
   // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
 }
-
 void loop() {
     getRadioMessage();
-    if (g_freeIndexMsg != 0) {
-        for (int i = 0; i < g_freeIndexMsg; i++) {
-            Serial.println(g_msgArray[i]);
-            g_msgArray[i] = String("");
-        }
-        g_freeIndexMsg = 0;
-    }
+    sendAllMessagesToPhone();
 }
